0cb35ee5ad0d2851242eb8087def50554fe4bbea
statelesshz <hzji210@gmail.com>
2023-11-24 20:41:02 +0800

    feat(pack-up-module): [3/n] lazy import

--- src/lazymodule/__init__.py
+++ src/lazymodule/__init__.py
@@ -1,12 +1,27 @@
-from .utils import is_tf_available, is_torch_available
+__version__ = "0.0.1"
 
+import sys
+from .utils import is_tf_available, is_torch_available, _LazyModule
+
+_import_structure = {
+    "utils": ["is_tf_available", "is_torch_available"],
+}
 
 if is_torch_available():
-    from .models import BertEmbeddings
+    _import_structure["models"] = ["BertEmbeddings"]
 else:
-    from .utils.dummy_pt import BertEmbeddings
+    _import_structure["utils.dummy_pt"] = ["BertEmbeddings"]
 
 if is_tf_available():
-    from .models import TFBertEmbeddings
+    _import_structure["models"] = ["TFBertEmbeddings"]
 else:
-    from .utils.dummy_tf import TFBertEmbeddings
+    _import_structure["utils.dummy_tf"] = ["TFBertEmbeddings"]
+
+
+sys.modules[__name__] = _LazyModule(
+    __name__,
+    globals()["__file__"],
+    _import_structure,
+    module_spec=__spec__,
+    extra_objects={"__version__": __version__},
+)
--- src/lazymodule/utils/__init__.py
+++ src/lazymodule/utils/__init__.py
@@ -1,3 +1,3 @@
-from .import_utils import is_tf_available, is_torch_available
+from .import_utils import is_tf_available, is_torch_available, _LazyModule
 from .dummy_tf import TFBertEmbeddings
 from .dummy_pt import BertEmbeddings
